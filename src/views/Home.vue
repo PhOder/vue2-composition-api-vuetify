2121f416e186ccd7102e7f2e26bb5ddd28cfa527
Philip Odermatt <philip.odermatt@accso.de>
2020-08-21 12:01:37 +0200

    initial prettier setup, replace Helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>{{ someTestData }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      someTestData: "Helloworld",
+    };
+  },
 });
 </script>
