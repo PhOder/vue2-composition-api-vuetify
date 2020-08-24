a295ada685441b8cc3e5205e97e22dd88a743a9a
Philip Odermatt <philip.odermatt@accso.de>
2020-08-24 10:30:04 +0200

    include useUser in Home page component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <v-container class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div>
+    <div v-if="loggedIn">
       <v-text-field label="Hello" v-model="someTestData"></v-text-field>
       <Test />
       <Test :msg="someTestData" />
     </div>
+    <div v-else>Please login first!</div>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "@vue/composition-api";
 import Test from "@/components/Test.vue";
+import useUser from "@/composables/useUser";
 
 export default defineComponent({
   name: "Home",
@@ -20,8 +22,10 @@ export default defineComponent({
   },
   setup() {
     const someTestData = ref("Helloworld");
+    const { loggedIn } = useUser();
     return {
       someTestData,
+      loggedIn,
     };
   },
 });
